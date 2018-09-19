3d22c1f63ace4a410a52008e87698a57ca21dd37
HH2233 <2573342359@qq.com>
2018-09-19 08:27:45 +0800

    123

---
+++ alpaca.java
@@ -0,0 +1,10 @@
+public class  animal{
+    public static void main(String[] args) {
+        System.out.println(this.alpaca);
+    }
+
+    public void mailCheck()
+    {
+        System.out.println("羊驼（拉丁学名：Vicugna pacos，英文名：alpaca）为偶蹄目、骆驼科的动物，体重55-65千克，头体长度1200-2250毫米。外形有点像绵羊，一般栖息于海拔4000米的高原。"）;
+    }
+}
